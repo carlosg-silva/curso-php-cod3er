580e2d5db7f78f254dfaa66b3afd938180d7ab2d
Carlos <cgomessilva2015@gmail.com>
2023-03-26 16:41:05 -0300

    final secao html & css

--- index.php
+++ index.php
@@ -17,9 +17,23 @@
     <h1>Curso PHP</h1>
     <h2>Índice dos Exercícios</h2>
   </header>
-  <main class="principal"></main>
+  <main class="principal">
+    <div class="conteudo">
+      <nav class="modulos">
+        <div class="modulo verde">
+          <h3>Módulo 01 - Básico</h3>
+          <ul>
+            <li><a href="exercicio.php?dir=teste&file=teste">Exercício A</a></li>
+
+          </ul>
+        </div>
+
+
+      </nav>
+    </div>
+  </main>
   <footer class="rodape">
-    COD3R & ALUNOS © 2023
+    COD3R & ALUNOS © <?= date('Y') ?>
   </footer>
 </body>
 
