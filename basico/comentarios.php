b87ed65d8157b3b9b93959088ac2091dc0c9faed
Carlos <cgomessilva2015@gmail.com>
2023-03-28 14:11:38 -0300

    secao integrando php/html/css - comentários

---
+++ basico/comentarios.php
@@ -0,0 +1,27 @@
+<div class="titulo">Comentários PHP</div>
+
+<?php
+echo "Estou no PHP";
+// echo "Comentário";
+# echo "Comentário";
+# echo // "Comentário";
+// echo # "Comentário";
+?>
+
+<p>Depois do primeiro bloco!</p>
+<!-- <p>Comentário HTML1</p>
+    <p>Comentário HTML2</p>
+    <p>Comentário HTML3</p>
+    <p>Comentário HTML4</p>
+-->
+
+
+<?php
+/* 
+  Linha 1 do comentário
+  Linha 2 do comentário
+  Linha 3 do comentário
+*/
+?>
+
+<p>Depois do primeiro bloco!</p>
