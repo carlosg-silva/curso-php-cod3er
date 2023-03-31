f360bad6992b929225199ff92a0ce2b5b2729d90
Carlos <cgomessilva2015@gmail.com>
2023-03-31 15:50:01 -0300

    secao variaveis aula interpolacao

--- index.php
+++ index.php
@@ -62,6 +62,12 @@
           <h3>Variáveis</h3>
           <ul>
             <li><a href="exercicio.php?dir=variaveis&file=basico">Variáveis</a></li>
+
+            <li><a href="exercicio.php?dir=variaveis&file=desafio_equacao">Desafio Equação</a></li>
+
+            <li><a href="exercicio.php?dir=variaveis&file=atribuicoes">Atribuições</a></li>
+
+            <li><a href="exercicio.php?dir=variaveis&file=interpolacao">Interpolação</a></li>
           </ul>
         </div>
       </nav>
--- variaveis/basico.php
+++ variaveis/basico.php
@@ -4,3 +4,34 @@
 $numeroA = 13;
 echo $numeroA . "<br>";
 var_dump($numeroA);
+
+echo "<br>";
+$a = 3;
+$b = 16;
+$somaDosNumeros = $a + $b;
+
+echo $somaDosNumeros;
+echo "<br>";
+echo isset($somaDosNumeros);
+echo "<br>";
+
+unset($somaDosNumeros);
+var_dump($somaDosNumeros);
+
+$variavel = 10;
+echo "<br>" . $variavel;
+
+$variavel = "Agora sou uma string!";
+echo "<br>" . $variavel;
+
+// Nomes de variável
+$var = "valida";
+$var2 = "valida";
+$VAR3 = "valida";
+$_var_ = "valida";
+//$6var = "invalida";
+// $%var7 = "valida";
+// $8var8 = "valida";
+
+echo "<br>";
+var_dump($_SERVER["HTTP_HOST"]);
